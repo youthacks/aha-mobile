c3656aba8d5377b4053d56b58aea527051c7037a
Matthew Soh <matthew@mattsoh.dev>
2025-07-04 13:57:57 -0700

    Initial Commit

---
+++ aha-mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aha-mobile
+//
+//  Created by Matthew Soh on 7/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aha-mobileTests/aha_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  aha_mobileTests.swift
+//  aha-mobileTests
+//
+//  Created by Matthew Soh on 7/4/25.
+//
+
+import Testing
+@testable import aha_mobile
+
+struct aha_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
