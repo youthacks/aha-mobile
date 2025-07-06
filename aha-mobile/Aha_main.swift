67faf9ffa98c003e98671d20a393e6ba1bd8660c
Matthew Soh <matthew@mattsoh.dev>
2025-07-06 14:45:14 -0700

    learn how to use storyboard

---
+++ aha-mobile/Aha_main.swift
@@ -0,0 +1,17 @@
+//
+//  Main.swift
+//  aha-mobile
+//
+//  Created by Matthew Soh on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Aha_main: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
