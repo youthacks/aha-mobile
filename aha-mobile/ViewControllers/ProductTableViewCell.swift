0437271c84be25658d64f8acdd92adf433089963
Matthew Soh <matthew@mattsoh.dev>
2025-07-09 13:56:39 -0700

    implement products page

---
+++ aha-mobile/ViewControllers/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  aha-mobile
+//
+//  Created by Matthew Soh on 7/8/25.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
